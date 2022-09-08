4569fbd1ec5a6a026e4ff3aa21eda812595acaa7
Safa Begum <safabegum@Safas-MacBook-Pro.local>
2022-09-07 21:24:25 -0400

    Selenium set up

---
+++ src/main/java/sep7/DemoClass.java
@@ -0,0 +1,15 @@
+package sep7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","driver/chromedriver");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://amazon.com");
+        String text= driver.getTitle();
+        System.out.println(text);
+
+    }
+}
